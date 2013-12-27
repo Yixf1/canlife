e13e276a62d30ed0bb1102b53a2da03735429a9d
Yixf1 <yixf1@163.com>
2013-12-27 12:19:42 +0800

    create project canlifen

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+$(function(){
+	$("#cart").hover(
+		function(){
+			$(this).next().show();
+			$(this).css({opacity: 0.8});
+		},
+		function(){
+			$(this).next().hide();
+			$(this).css({opacity: 1});
+		}
+	);
+	$("#cartPanel").hover(
+		function(){
+			$(this).show();
+			$(this).prev().css({opacity: 0.8});
+		},
+		function(){
+			$(this).hide();
+			$(this).prev().css({opacity: 1});
+		}
+	);
+	$("#userInfo").hover(
+		function(){
+			$(this).next().show();
+			$(this).css({opacity: 0.8});
+		},
+		function(){
+			$(this).next().hide();
+			$(this).css({opacity: 1});
+		}
+	);
+	$("#userPopupCon").hover(
+		function(){
+			$(this).show();
+			$(this).prev().css({opacity: 0.8});
+		},
+		function(){
+			$(this).hide();
+			$(this).prev().css({opacity: 1});
+		}
+	);
+	$("input[type=text]").focus(function(){
+		if(typeof($(this).attr("defaultText"))!="undefined" && $(this).attr("defaultText") == $(this).val()){
+			$(this).val("");
+		}
+	}).blur(function(){
+		if(typeof($(this).attr("defaultText"))!="undefined" && $(this).val()==""){
+			$(this).val($(this).attr("defaultText"));
+		}
+	});
+});
